ef004e6d872a3059ac037c34171dc01775a48667
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-14 10:46:42 +0200

    Update helper_funcs.c
    Created the strcmp function for use in the Shell

--- helper_funcs.c
+++ helper_funcs.c
@@ -19,3 +19,25 @@ int _strlen(char *str)
 	}
 	return (len);
 }
+
+/**
+ * _strcmp - compare 2 strings
+ * @s1: 1st string to compare
+ * @s2: 2nd string to compare
+ * Return: 0 if equal, < 0 if s1 < s2, > 0 if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
+	i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
