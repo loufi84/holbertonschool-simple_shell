54c59ee44a0dadad2e5f618197d66a7546bec721
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-14 11:34:05 +0200

    Update helper_funcs
    Updated helper_funcs.c with a atoi function

--- helper_funcs.c
+++ helper_funcs.c
@@ -41,3 +41,47 @@ int _strcmp(char *s1, char *s2)
 
 	return (s1[i] - s2[i]);
 }
+
+/**
+ * _atoi - convert a string to an itnt
+ * @s: sring to convert to int
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int num = 0;
+	int sign = 1;
+	int fnd_dig = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			fnd_dig = 1;
+		}
+		else if (fnd_dig)
+		{
+			break;
+		}
+
+		i++;
+	}
+
+	if (!fnd_dig)
+		return (0);
+
+	return (num * sign);
+}
