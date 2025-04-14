7e383d550e65015556b287fad2da5e4e74ac625c
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-14 10:13:03 +0200

    Created skeleton of project
    Created the header, main.c and gitignore for the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # holbertonschool-simple_shell
  The repo for Holberton Simple Shell
+This is a placeholder for now
---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: The argument count
+ * @argv: Value of arguments
+ *
+ * Return: 0 for success, various errors
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
