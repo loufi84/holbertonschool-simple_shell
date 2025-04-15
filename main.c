9dc1f7881a93c02b8d4fedd9077dd9e4802dd6fe
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-15 14:19:59 +0200

    Nothing to see here

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * @argc: The argument count
- * @argv: Value of arguments
- *
  * Return: 0 for success, various errors
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 
 }
