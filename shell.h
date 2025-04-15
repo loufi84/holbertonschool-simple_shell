ac726db66ed5c9e1fb54fe391b48b53cd9ace850
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-15 14:23:38 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,26 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 1024
+
+extern char **environ;
+
+
+
+#endif /* SHELL_H */
 
 #endif /* SHELL_H */
