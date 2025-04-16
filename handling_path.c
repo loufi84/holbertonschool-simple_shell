062705a58902243b6b2313d504ed07cc99c93945
Delphes1980 <delphine.coutouly-laborda@hotmail.com>
2025-04-16 19:20:30 +0200

    divise is_built_in function into 2: is_absolute and handling_path files

---
+++ handling_path.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+* handling_path - handles the path if not absolute
+* @cmd: an array of tokens (= input)
+*
+* Return: 0 in success, -1 in failure
+*/
+
+int handling_path(char **cmd)
+{
+	static char buffer[BUFFER_SIZE];
+	char **dir;
+	char *path_env = getenv("PATH");
+	int full_path = split_string(path_env, dir);/*Look for the path*/
+	int i = 0;
+
+	if (cmd == NULL || *cmd == NULL)/*Check if pointers are valid*/
+	{
+		return (-1);
+	}
+
+	/*Check if PATH is absent or here but empty*/
+	if (path_env == NULL || path_env[0] == '\0')
+	{
+		return (-1);
+	}
+
+	while (full_path)
+	{
+		sprintf(buffer, "%s", *cmd);
+
+		if (access(buffer, F_OK) && access(buffer, X_OK))
+		{
+			return (buffer);
+		}
+
+		else
+		{
+			return (-1);
+		}
+
+		i++;
+	}
+
+	return (0);
+}
---
+++ is_built_in.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+* is_built_in - check if the command is an absolute path or a built_in
+* @cmd: an array of tokens (= input)
+*
+* Return: 0 in success, -1 in failure
+*/
+
+int is_built_in(char **cmd)
+{
+	builtin_t *verif;
+	int i = 0;
+	char *path_env = getenv("PATH");
+
+	if (cmd == NULL || *cmd == NULL)/*Check if pointers are valid*/
+	{
+		return (-1);
+	}
+
+	while (verif != NULL)
+	{/*If cmd is a built-in, call the correct function*/
+		if (strcmp(verif[i].name, cmd[0]) == 0)
+		{
+			verif[i].func;
+			return (0);
+		}
+
+		i++;
+	}
+
+	if (strcmp(cmd[0], "/") == 0)/*Check if cmd is an absolute path*/
+	{/*If path exists and if it's executable, execute the input*/
+		if (access(path_env, F_OK) && access(path_env, X_OK))
+		{
+			execve(path_env, cmd, environ);
+		}
+
+		else
+		{
+			return (-1);
+		}
+	}
+
+	else/*If not a built-in nor an absolute path*/
+	{
+		return (-1);
+	}
+}
